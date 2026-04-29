13d05b4ed1c4a7fb26b6971ed0bfa2b3929f6484
Fabrizio Tomas albanese <falbanese@frba.utn.edu.ar>
2026-04-29 17:05:35 -0300

    Update hello.cpp

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -2,5 +2,8 @@
 
 int main() {
     std::cout << "Hello, World!\n";
+    std::cout << "Nombre y apellido: Fabrizio Tomas Albanese\n";
+    std::cout << "Curso de A.y E.D.: K1053\n";
+    std::cout << "Grupo asignado: 6\n";
   return 0;
 }
